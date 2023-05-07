862345a5dde5f45996f181ba500c48bfe0c686c7
antos07 <trotsenko.ant07@gmail.com>
2023-05-07 14:05:11 +0200

    Move all the backend code into "backend" folder

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for notice_board project.
+ASGI config for backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notice_board.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
