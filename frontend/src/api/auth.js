b6fc597aaf1d2364f02814abdf36918ee4576e10
antos07 <trotsenko.ant07@gmail.com>
2023-05-08 10:49:34 +0200

    Add support of axios-jwt

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,29 +1,12 @@
 import client from './client'
+import {clearAuthTokens, setAuthTokens} from "axios-jwt";
 
 export async function login(username, password) {
     const response = await client.post('/api/jwt/create/', {username: username, password: password})
-    const {accessToken, refreshToken} = response.data;
 
-    localStorage.setItem('access', accessToken)
-    localStorage.setItem('refresh', refreshToken)
-    setAuthHeader(accessToken)
+    setAuthTokens({accessToken: response.data.access, refreshToken: response.data.refresh})
 }
 
 export async function logout() {
-    localStorage.removeItem('access')
-    localStorage.removeItem('refresh')
-}
-
-export async function refreshToken() {
-    const refreshToken = localStorage.getItem('refresh')
-
-    const response = await client.post('/api/jwt/refresh/', {refresh: refreshToken})
-    const {accessToken} = response.data;
-
-    localStorage.setItem('access', accessToken)
-    setAuthHeader(accessToken)
-}
-
-function setAuthHeader(accessToken) {
-    client.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+    clearAuthTokens()
 }
--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import {applyAuthTokenInterceptor} from "axios-jwt";
+
+const BASE_URL = "http://localhost:8000"
 
 const client = axios.create({
     baseURL: "http://localhost:8000"
+})
+
+applyAuthTokenInterceptor(client, {
+    requestRefresh: async (refreshToken) => {
+        const url = `${BASE_URL}/api/jwt/refresh`
+        const response = await axios.post(url, {refresh: refreshToken})
+        return response.data.refresh
+    }
 })
 
 export default client
